8e2119c3de323c32786ded0d39519e8d5fef7c8e
mohammad ramezani <mohammad0ramezani@outlook.com>
2019-09-19 09:45:42 +0430

    change menu 13980625

--- MYShop.Web.UI/Components/MenuViewComponent.cs
+++ MYShop.Web.UI/Components/MenuViewComponent.cs
@@ -9,14 +9,16 @@ namespace MYShop.Web.UI.Components
 {
     public class MenuViewComponent : ViewComponent
     {
+        private readonly IBrandRepository _repo1;
         private readonly ICategoryRepository _repo;
-        public MenuViewComponent(ICategoryRepository repo)
+        public MenuViewComponent(ICategoryRepository repo, IBrandRepository repo1)
         {
             _repo = repo;
+            _repo1 = repo1;
         }
         public IViewComponentResult Invoke()
         {
-
+            ViewBag.brand = _repo1.GetBrands();
             return View("View", _repo.GetAll());
         }
     }
