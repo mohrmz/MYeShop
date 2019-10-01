f7e3d5acb47f40ab7d78f72cc1c95bc25e8ca128
mohammad ramezani <mohammad0ramezani@outlook.com>
2019-10-01 21:37:21 +0330

    Revert "13980707"
    This reverts commit 2bdeef8e7454a5e9b5c918aea92873aa99693c02.

--- MYShop.Web.UI/Components/MenuViewComponent.cs
+++ MYShop.Web.UI/Components/MenuViewComponent.cs
@@ -11,17 +11,14 @@ namespace MYShop.Web.UI.Components
     {
         private readonly IBrandRepository _repo1;
         private readonly ICategoryRepository _repo;
-        private readonly IProductRepository _repo2;
-        public MenuViewComponent(ICategoryRepository repo, IBrandRepository repo1, IProductRepository repo2)
+        public MenuViewComponent(ICategoryRepository repo, IBrandRepository repo1)
         {
             _repo = repo;
             _repo1 = repo1;
-            _repo2 = repo2;
         }
         public IViewComponentResult Invoke()
         {
             ViewBag.brand = _repo1.GetBrands();
-            ViewBag.products = _repo2.GetAll();
             return View("View", _repo.GetAll());
         }
     }
